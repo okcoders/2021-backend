24e2c202040a197a6fe09a5ff1a91dac0fafc286
Devon <devon.mobley@gmail.com>
2021-09-02 06:46:16 -0500

    week 3 start

--- week2/test.js
+++ week2/test.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 const { User } = require('./schemas/schemas');
 const dotenv = require('dotenv');
 dotenv.config();
-console.log(process.env.DB_URI);
-
 /* ----- Config/Init -------*/
 main()
   .then(() => {
@@ -22,5 +20,8 @@ main()
   });
 
 async function main() {
-  await mongoose.connect();
+  `mongodb://myDBReader:D1fficultP%40ssw0rd@mongodb0.example.com:27017/?authSource=admin`;
+  await mongoose.connect(
+    `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}?authSource=admin`
+  );
 }
